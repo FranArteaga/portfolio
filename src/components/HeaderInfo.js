4f87e8b2640be9ff3b8978696cc184f981f668df
francis <franrtg96@gmail.com>
2023-07-09 18:06:08 -0600

    improved spacing in tailwind and added font poppins

--- src/components/HeaderInfo.js
+++ src/components/HeaderInfo.js
@@ -4,14 +4,14 @@ import { BsLinkedin } from 'react-icons/bs'
 
 export default function HeaderInfo() {
     return (
-        <header className='w-full md:flex md:pt-4 md:p-6'>
-            <section className='md:w-[50%] p-12' >
+        <header className='w-full md:flex md:pt-4 md:px-10'>
+            <section className='md:w-[50%] p-6' >
                 <h1 className='text-[2.5em] font-bold py-1'>Francisco Arteaga</h1>
                 <h4 className='text-2xl font-medium'>FullStack Developer</h4>
-                <p className='text-xl py-4'>Proactive and resourceful web developer.
+                <p className='text-xl py-4 text-gray-600'>Proactive and passionate web developer.
                     Experienced in building Web applications with
-                    JavaScript / Reactjs / Nodejs / Reactjs / Bootstrap / TailwindCSS </p>
-                <div className='w-[50%] py-4 flex justify-around items-center '>
+                    JavaScript / Reactjs / Nodejs / Nextjs / Bootstrap / TailwindCSS </p>
+                <div className='md:w-[50%] py-4 flex justify-around items-center '>
                     <BsGithub className='text-[2em]'/>
                     <BsLinkedin className='text-[2em]'/>
                     <button className='text-white text-sm font-bold p-[.45em] bg-black  rounded-lg'>
