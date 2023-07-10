1985808892e483421a244b4d2d822174a22e47f2
francis <franrtg96@gmail.com>
2023-07-09 21:27:36 -0600

    slight changes in paddings and added spacing in paragraph

--- src/components/HeaderInfo.js
+++ src/components/HeaderInfo.js
@@ -12,24 +12,25 @@ export default function HeaderInfo() {
             <section className='md:w-[50%] p-6' >
                 <h1 className='text-[2.5em] font-bold py-1'>Francisco Arteaga</h1>
                 <h4 className='text-2xl font-medium'>FullStack Developer</h4>
-                <p className='text-xl py-4 text-gray-600'>Proactive and passionate web developer.
+                <p className='text-xl py-4 text-gray-600 leading-loose'>
+                    Proactive and passionate web developer.
                     Experienced in building Web applications with
                     JavaScript / Reactjs / Nodejs / Nextjs / Bootstrap / TailwindCSS </p>
-                <div className='md:w-[50%] py-4 flex justify-around items-center '>
+                <div className='md:w-[50%] py-6 flex justify-around items-center'>
                     <Link href={GithubLink}>
-                        <BsGithub className='text-[2em]' />
+                        <BsGithub className='text-[2.5em]' />
                     </Link>
                     <Link href={LinkedinLink}>
-                        <BsLinkedin className='text-[2em]' />
+                        <BsLinkedin className='text-[2.5em]' />
                     </Link>
-                    <button className='text-white text-sm font-bold p-[.45em] bg-black  rounded-lg'>
+                    <button className='text-white text-sm font-bold p-[.72em] bg-black rounded-md'>
                         My Resume
                     </button>
                 </div>
             </section>
 
             <section className='md:w-[50%] grid'>
-                <img src="./office.png" className='md:w-[75%] place-self-center' />
+                <img src="./office.png" className='md:w-[70%] place-self-center' />
             </section>
 
         </header>
