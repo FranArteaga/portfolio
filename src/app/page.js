10b2a6b2a3e9ee56e3a678fbefd191f62b192eff
francis <franrtg96@gmail.com>
2023-07-09 22:27:43 -0600

    initialized skills section branch

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import HeaderInfo from '@/components/HeaderInfo'
 import { Poppins } from '@next/font/google'
+import SkillsSection from '@/components/SkillsSection'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -15,6 +16,7 @@ export default function Home() {
       <main className={poppins.className}>
         <Navbar />
         <HeaderInfo />
+        <SkillsSection/>
       </main>
     </>
   )
