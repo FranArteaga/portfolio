a43dbcd239f89685ba9acfe08d11ee8ff2576778
francis <franrtg96@gmail.com>
2023-07-10 17:59:43 -0600

    defined first structure for skills section

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 export default function SkillsSection() {
   return (
-    <section>
-      skills-Section
+    <section className='h-[100vh] flex pt-28'>
+
+      <section className='w-[50%] grid'>
+        <img src="./coding.png" className='w-[70%] place-self-center ' />
+      </section>
+
+      <section className='w-[50%]'>
+        <h2 className=' text-[2em] font-sm'>
+          Skills and knowledge
+        </h2>
+        <p>
+          FullStack Developer open to work with new technologies
+        </p>
+        <span className=''>
+          icons
+        </span>
+      </section>
+
     </section>
 
   )
