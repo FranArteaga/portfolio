b9db6d36e7209d09570870ac410961bf159f2e53
francis <franrtg96@gmail.com>
2023-07-12 23:58:42 -0600

    added icons with links to project's code and page

--- src/components/CardForProject.js
+++ src/components/CardForProject.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
+import { BiLinkExternal } from 'react-icons/bi';
+import { AiFillGithub } from 'react-icons/ai';
 
 export default function CardForProject() {
   return (
     <div className='md:flex bg-zinc-100 rounded-md drop-shadow-lg'>
+
         <section className='md:w-[55%] p-4 md:p-8 grid'>
-         <img src="./toknow-landing.png" className='rounded-md place-self-center drop-shadow-md'/>
+         <img src="./toknow-landing.png" className='rounded-md md:place-self-center drop-shadow-md'/>
         </section>
         
         <section className='md:w-[45%] p-8 pt-2 md:p-8'>
@@ -14,7 +18,18 @@ export default function CardForProject() {
             parent accounts and link them to a group and student,
             create scholar or group announcements and allow teachers 
             and parents to see them and commentate. </p>
+            <div className='pt-4 flex justify-center md:justify-end gap-8 items-end  '>
+                <Link href="https://github.com/to-know-g19/TOKNOW_FE" className='hover:text-indigo-500 duration-300 flex items-center gap-2'>
+                    Repository
+                    <AiFillGithub className='text-[2em]'/>
+                </Link >
+                <Link href="https://toknow.online/" className='hover:text-indigo-500 duration-300 flex items-center gap-2'>
+                    Page
+                    <BiLinkExternal className='text-[2em]'/>
+                </Link >
+            </div>
         </section>
+        
     </div>
   )
 }
