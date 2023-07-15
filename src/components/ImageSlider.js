cba6b2a52922029d945dcebc6566220153c0e99f
francis <franrtg96@gmail.com>
2023-07-15 02:18:09 -0600

    added dots below image with click function to show corresponding image

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
+import { RxDotFilled } from 'react-icons/rx'
 
 export default function ImageSlider() {
     const slides = [
@@ -25,9 +26,14 @@ export default function ImageSlider() {
         const newIndex = isLastSlide ? 0 : currentIndex + 1
         setCurrentIndex(newIndex)
     }
+
+    const goToSlide = (slideIndex) =>{
+        setCurrentIndex(slideIndex)
+    }
+
     return (
         <div className='max-w-[1080px] h-[720px] w-full m-auto py-16 px-4 relative group '>
-            <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
+            <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='w-full h-full rounded-2xl bg-center bg-cover duration-300'>
 
                 <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
                     <BsChevronCompactLeft onClick={prevSlide} />
@@ -35,8 +41,18 @@ export default function ImageSlider() {
                 <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
                     <BsChevronCompactRight onClick={nextSlide} />
                 </div>
-
             </div>
+
+                <div className='flex top-4 justify-center py-2'>
+                    {slides.map((slide, slideIndex) => (
+                        <div 
+                        key={slideIndex} 
+                        onClick={() => goToSlide(slideIndex) } 
+                        className='text-xl cursor-pointer'>
+                            <RxDotFilled/>
+                        </div>
+                    ))}
+                </div>   
         </div>
     )
 }
