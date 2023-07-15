f71d07d2048bc77cad74d2b42761608ed013ce68
francis <franrtg96@gmail.com>
2023-07-15 02:02:19 -0600

    added functions to make the slides work on click

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -13,15 +13,27 @@ export default function ImageSlider() {
             url: "https://queretaro.tecnm.mx/wp-content/uploads/2021/11/placeholder.png"
         },
     ]
-    return (
-        <div className='max-w-[1080px] h-[720px] w-full m-auto py-16 px-4 relative'>
-            <div style={{ backgroundImage: `url(${slides[2].url})` }} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
 
-                <div className='absolute top-[50%] -translate-x-0 -translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 textwhite cursor-pointer'>
-                    <BsChevronCompactLeft />
+    const [currentIndex, setCurrentIndex] = useState(0)
+    const prevSlide = () => {
+        const isFirstSlide = currentIndex === 0
+        const newIndex = isFirstSlide ? slides.length -1 : currentIndex - 1
+        setCurrentIndex(newIndex)
+    }
+    const nextSlide = () => {
+        const isLastSlide = currentIndex === slides.length -1
+        const newIndex = isLastSlide ? 0 : currentIndex + 1
+        setCurrentIndex(newIndex)
+    }
+    return (
+        <div className='max-w-[1080px] h-[720px] w-full m-auto py-16 px-4 relative group '>
+            <div style={{ backgroundImage: `url(${slides[currentIndex].url})` }} className='w-full h-full rounded-2xl bg-center bg-cover duration-500'>
+
+                <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <BsChevronCompactLeft onClick={prevSlide} />
                 </div>
-                <div className='absolute top-[50%] -translate-x-0 -translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 textwhite cursor-pointer'>
-                    <BsChevronCompactRight />
+                <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 -translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
+                    <BsChevronCompactRight onClick={nextSlide} />
                 </div>
 
             </div>
