e85ccd09d554cf2b6b9fa92a9b5fb8a9f63808e7
francis <franrtg96@gmail.com>
2023-07-17 18:45:49 -0600

    changed hover colors, added z-index  and fixed nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
 
   return (
     <>
-      <navbar className="text-xl md:bg-white p-5 md:flex md:items-center md:justify-between ">
+      <navbar className="z-index-[1] drop-shadow-md fixed w-full text-xl bg-white p-5 md:flex md:items-center md:justify-between ">
 
         <section className='px-4 flex justify-between items-center text-center'>
           <p>Francisco Arteaga</p>
@@ -20,16 +20,16 @@ export default function Navbar() {
 
         <ul className="md:flex md:items-center z-[1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7">
           <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-red-600 duration-500'>Skills</Link>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Skills</Link>
           </li>
           <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-red-600 duration-500'>Projects</Link>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Projects</Link>
           </li>
           <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-red-600 duration-500'>Hobbies</Link>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Hobbies</Link>
           </li>
           <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-red-600 duration-500'>Contact</Link>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Contact</Link>
           </li>
         </ul>
 
