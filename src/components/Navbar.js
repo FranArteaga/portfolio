40afa3c2a5a073149144c36c553d19e8c307449f
francis <franrtg96@gmail.com>
2023-07-20 02:14:57 -0600

    fixed key prop in lists and centered menu button in mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,18 +15,18 @@ export default function Navbar() {
     <>
 
       <div className='shadow-md w-full fixed top-0 left-0'>
-        <div className='md:flex items-center justify-between bg-white py-4 md:px-10 px-7'>
+        <div className='flex items-center justify-between bg-white py-4 md:px-10 px-7'>
           <div className='text-xl cursor-pointer flex items-center  text-gray-800'>
             Francisco Arteaga
           </div>
-          <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+          <div onClick={()=>setOpen(!open)} className='text-3xl flex items-center right-8 top-6 cursor-pointer md:hidden'>
             <FiMenu/>
           </div>
 
           <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in-out  ${open ? 'top-12 opacity-100': 'top-[-500px]' } `}>
             {
               arrLinks.map((aLink) => (
-                <li key={Link.name} className='md:ml-8 text-xl md:my-0 my-5'>
+                <li key={aLink.name} className='md:ml-8 text-xl md:my-0 my-5'>
                   <Link href={aLink.link} className='hover:text-indigo-500 duration-300'>{aLink.name}</Link>
 
                 </li>
