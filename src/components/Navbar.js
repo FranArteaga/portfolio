4cf8d17b0d5d47a1bfd348d15ea45d1bf11de42a
francis <franrtg96@gmail.com>
2023-07-20 02:04:54 -0600

    menu button shows and works on mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
     { name: 'Hobbies', link: '/' },
     { name: 'Contact', link: '/' },
   ]
-
+  let [open, setOpen] = useState(false)
   return (
     <>
 
@@ -19,15 +19,16 @@ export default function Navbar() {
           <div className='text-xl cursor-pointer flex items-center  text-gray-800'>
             Francisco Arteaga
           </div>
-          <div className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
-          <FiMenu />
+          <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
+            <FiMenu/>
           </div>
-          <ul className='md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in'>
+
+          <ul className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-300 ease-in-out  ${open ? 'top-12 opacity-100': 'top-[-500px]' } `}>
             {
-              arrLinks.map((aLink)=> (
+              arrLinks.map((aLink) => (
                 <li key={Link.name} className='md:ml-8 text-xl md:my-0 my-5'>
                   <Link href={aLink.link} className='hover:text-indigo-500 duration-300'>{aLink.name}</Link>
-                  
+
                 </li>
               ))
             }
@@ -35,7 +36,33 @@ export default function Navbar() {
         </div>
 
       </div>
+      {/* <navbar className="z-index-[1] drop-shadow-md fixed w-full text-xl bg-white p-5 md:flex md:items-center md:justify-between ">
 
+        <section className='px-4 flex justify-between items-center text-center'>
+          <p>Francisco Arteaga</p>
+
+          <span className='text-2xl cursor-pointer md:hidden block'>
+            <FiMenu/>
+          </span>
+
+        </section>
+
+        <ul className="md:flex md:items-center z-[1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7">
+          <li className='px-4 my-6 md:my-0'>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Skills</Link>
+          </li>
+          <li className='px-4 my-6 md:my-0'>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Projects</Link>
+          </li>
+          <li className='px-4 my-6 md:my-0'>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Hobbies</Link>
+          </li>
+          <li className='px-4 my-6 md:my-0'>
+            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Contact</Link>
+          </li>
+        </ul>
+
+      </navbar> */}
     </>
   )
 }
