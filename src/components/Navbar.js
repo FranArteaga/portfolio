9f36640aa29fcca30b1eab658d326a7934467f6f
francis <franrtg96@gmail.com>
2023-07-20 03:09:49 -0600

    deleted commented lines

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,6 @@ export default function Navbar() {
               arrLinks.map((aLink) => (
                 <li key={aLink.name} className='md:ml-8 text-xl md:my-0 my-5'>
                   <Link href={aLink.link} className='hover:text-indigo-500 duration-300'>{aLink.name}</Link>
-
                 </li>
               ))
             }
@@ -39,33 +38,6 @@ export default function Navbar() {
 
         </div>
       </div>
-      {/* <navbar className="z-index-[1] drop-shadow-md fixed w-full text-xl bg-white p-5 md:flex md:items-center md:justify-between ">
-
-        <section className='px-4 flex justify-between items-center text-center'>
-          <p>Francisco Arteaga</p>
-
-          <span className='text-2xl cursor-pointer md:hidden block'>
-            <FiMenu/>
-          </span>
-
-        </section>
-
-        <ul className="md:flex md:items-center z-[1] md:z-auto md:static absolute bg-white w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7">
-          <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Skills</Link>
-          </li>
-          <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Projects</Link>
-          </li>
-          <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Hobbies</Link>
-          </li>
-          <li className='px-4 my-6 md:my-0'>
-            <Link href={"#"} className='hover:text-indigo-500 duration-300'>Contact</Link>
-          </li>
-        </ul>
-
-      </navbar> */}
     </>
   )
 }
