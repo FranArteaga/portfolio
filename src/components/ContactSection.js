4c231982bb4305694c912455ce34128a8696f0e2
francis <franrtg96@gmail.com>
2023-08-07 22:19:43 -0600

    added subtitle to section

--- src/components/ContactSection.js
+++ src/components/ContactSection.js
@@ -5,7 +5,8 @@ import ContactCard from './ContactCard'
 export default function ContactSection() {
     return (
         <section id="section-Contact" className=' pt-16 sm:pt-20 md:pt-10'>
-            <h2 className='text-[2em] py-6'>Contact Me</h2>
+            <h2 className='text-[2em] font-bold py-6'>Contact Me</h2>
+            <h4 className='text-xl text-gray-600 leading-loose pb-8'> Currently looking for opportunities. Feel free to send me a message! </h4>
             <div className='md:flex gap-8'>
 
                 <ContactCard
