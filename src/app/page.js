752266a4567058d8679199d00fc3434c87d168d2
francis <franrtg96@gmail.com>
2023-08-08 19:19:20 -0600

    created footer component and added it to the page

--- src/app/page.js
+++ src/app/page.js
@@ -7,23 +7,25 @@ import SkillsSection from '@/components/SkillsSection'
 import ProjectsSection from '@/components/ProjectsSection'
 import ImageSlider from '@/components/ImageSlider'
 import ContactSection from '@/components/ContactSection'
+import Footer from '@/components/Footer'
 
 const poppins = Poppins({
   subsets: ['latin'],
-  weight: ['400', '500', '700']
+  weight: ['400', '500', '600', '700']
 })
 export default function Home() {
 
   return (
     <>
       <main className={poppins.className}>
-          <Navbar />
+        <Navbar />
         <div className='py-16 md:pt-0 md:px-16 px-8'>
           <HeaderInfo />
           <SkillsSection />
-          <ProjectsSection/>
-          <ContactSection/>
+          <ProjectsSection />
+          <ContactSection />
         </div>
+        <Footer />
       </main>
     </>
   )
