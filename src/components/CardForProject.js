38a99c0c41ebe3a46809ff074233f2737ef6c787
francis <franrtg96@gmail.com>
2023-08-08 21:52:11 -0600

    made fonts in projectSection bolder

--- src/components/CardForProject.js
+++ src/components/CardForProject.js
@@ -14,18 +14,18 @@ export default function CardForProject() {
         </section>
         
         <section className='md:w-[45%] p-8 pt-2 md:p-8'>
-            <h3 className='text-[1.5em]'>Toknow</h3>
-            <p className='py-2'>School and groups management platform.</p>
+            <h3 className='text-[1.5em] font-semibold'>Toknow</h3>
+            <p className='py-2 text-[1.2em] font-medium'>School and groups management platform.</p>
             <p className='text-gray-700 leading-loose'>Create groups, add students, create teacher and
             parent accounts and link them to a group and student,
             create scholar or group announcements and allow teachers 
             and parents to see them and commentate. </p>
             <div className='pt-4 flex justify-center md:justify-end gap-8 items-end  '>
-                <Link href="https://github.com/to-know-g19/TOKNOW_FE" className='hover:text-indigo-500 duration-300 flex items-center gap-2'>
+                <Link href="https://github.com/to-know-g19/TOKNOW_FE" className='font-medium hover:text-indigo-600 duration-300 flex items-center gap-2'>
                     Repository
                     <AiFillGithub className='text-[2em]'/>
                 </Link >
-                <Link href="https://toknow.online/" className='hover:text-indigo-500 duration-300 flex items-center gap-2'>
+                <Link href="https://toknow.online/" className='font-medium hover:text-indigo-600 duration-300 flex items-center gap-2'>
                     Page
                     <BiLinkExternal className='text-[2em]'/>
                 </Link >
